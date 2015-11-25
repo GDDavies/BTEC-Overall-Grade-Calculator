293b29908f201388f359cd79417e8a2f8910359e
gdavies8 <gdaviesdev@gmail.com>
2015-11-25 22:00:39 +0000

    Formatting
    After rewriting in AJAX formatting was lost. Formatting has now been
    fixed.

--- getunit.php
+++ getunit.php
@@ -1,21 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-table {
-    width: 100%;
-    border-collapse: collapse;
-}
-
-table, td, th {
-    border: 1px solid black;
-    padding: 5px;
-}
-
-th {text-align: left;}
-</style>
+<head>
+ 
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script> 
+            <script src="http://malsup.github.com/jquery.form.js"></script> 
+	</head>
 </head>
 <body>
+    
+    
 
 <?php
 $q = intval($_GET['q']);
@@ -29,28 +23,40 @@ mysqli_select_db($con,"AllUnitData");
 $sql="SELECT * FROM mandatory_units_award WHERE sector_id = '".$q."'";
 $result = mysqli_query($con,$sql);
 
-echo "<table>
+?>
+    
+<div id="customers" class="alt">
+    <br>
+    <h3>Mandatory Units</h3>
+
+<table class="alt">
+    <thead>
 <tr>
 <th>Sector</th>
 <th>Unit Name</th>
 <th>GLH</th>
-<th>Assessment Type</th>
+<th>Assessment</th>
 <th>Unit Type</th>
-</tr>";
-
+</tr>
+</thead>
+    
+<?php
 while($row = mysqli_fetch_array($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['sector'] . "</td>";
-    echo "<td>" . $row['unit_name'] . "</td>";
-    echo "<td>" . $row['GLH'] . "</td>";
-    echo "<td>" . $row['assessment_type'] . "</td>";
-    echo "<td>" . $row['unit_type'] . "</td>";
-    echo "</tr>";
+    echo '<tbody>';
+    echo '<tr>';
+    echo '<td style="width:150px">' . $row['sector'] . '</td>';
+    echo '<td style="width:500px">' . $row['unit_name'] . '</td>';
+    echo '<td style="width:35px">' . $row['GLH'] . '</td>';
+    echo '<td style="width:50px">' . $row['assessment_type'] . '</td>';
+    echo '<td style="width:35px">' . $row['unit_type'] . '</td>';
+    echo '</tr>';
+    echo '</tbody>';
 }
 echo "</table>";
 mysqli_close($con);
 ?>
-    
+    </table>
+    </div>
 <?php
 $q = intval($_GET['q']);
 
@@ -62,29 +68,43 @@ if (!$con) {
 mysqli_select_db($con,"AllUnitData");
 $sql="SELECT * FROM optional_units_award WHERE sector_id = '".$q."'";
 $result = mysqli_query($con,$sql);
+?>
+<div id="customers" class="alt">
+    <br>
+    <h3>Optional Units</h3>
 
-echo "<table>
-<tr>
-<th>Sector</th>
-<th>Unit Name</th>
-<th>GLH</th>
-<th>Assessment Type</th>
-<th>Unit Type</th>
-<th>Select</th>
-</tr>";
+    <table class="alt">
+    <thead
+    <tr>
+    <th>Sector</th>
+    <th>Unit Name</th>
+    <th>GLH</th>
+    <th>Assessment</th>
+    <th>Unit Type</th>
+    <th>Select</th>
+    </tr>
+    </thead>
 
+    <?php
 while($row = mysqli_fetch_array($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['sector'] . "</td>";
-    echo "<td>" . $row['unit_name'] . "</td>";
-    echo "<td>" . $row['GLH'] . "</td>";
-    echo "<td>" . $row['assessment_type'] . "</td>";
-    echo "<td>" . $row['unit_type'] . "</td>";
-    echo'<td class="center"><input type="checkbox" name="includeUnit"></td>';
-    echo "</tr>";
+    echo '<tbody>';
+    echo '<tr>';
+    echo '<td style="width:150px">' . $row['sector'] . '</td>';
+    echo '<td style="width:500px">' . $row['unit_name'] . '</td>';
+    echo '<td style="width:35px">' . $row['GLH'] . '</td>';
+    echo '<td style="width:50px">' . $row['assessment_type'] . '</td>';
+    echo '<td style="width:35px">' . $row['unit_type'] . '</td>';
+    echo '<td class="center"><input type="checkbox" name="includeUnit"></td>';
+    echo '<tr>';
+    echo '</tbody>';
 }
 echo "</table>";
 mysqli_close($con);
 ?>
+    
+    </table>
+
+
+    
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <html>
 <head>
+    <meta charset="utf-8">
+		<title>BTEC (NQF) Overall Grade Calculator</title>
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900|Quicksand:400,700|Questrial" rel="stylesheet" />
+        <link href="default.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
 <script>
 function showUser(str) {
     if (str == "") {
@@ -24,17 +32,35 @@ function showUser(str) {
 }
 </script>
 </head>
+    
 <body>
-
+    
+    <div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+        	<!--<span class="icon icon-cog"></span>-->
+			<h1><a href="#">BTEC (NQF) Firsts<br>Overall Grade Calculator</a></h1>
+		</div>
+		<div id="menu">
+			<ul>
+				<li class="active"><a href="#" accesskey="1" title="">Calculator</a></li>
+				<li><a href="#" accesskey="2" title="">Guidance</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+    
 <form>
+<div class="wrapper">
+<div id="welcome" class="container">
 <select name="users" onchange="showUser(this.value)">
     <option value="">Select a Unit:</option>
     <option value="1">Animal Care</option>
     <option value="2">Application of Science</option>
     <option value="3">Art and Design</option>
     <option value="4">Business</option>
-    <option value="5">CPLD</option>
-    <option value="6">Construction and the Built Environment</option>
+    <option value="5">Construction and the Built Environment</option>
+    <option value="6">CPLD</option>
     <option value="7">Creative Digital Media Production</option>
     <option value="8">Engineering</option>
     <option value="9">Health and Social Care</option>
@@ -47,9 +73,19 @@ function showUser(str) {
     <option value="16">Sport</option>
     <option value="17">Travel and Tourism</option>
   </select>
+    </div>
 </form>
 <br>
-<div id="txtHint"><b>Person info will be listed here...</b></div>
+    <div id="txtHint"><b></b></div></div>
+    <div id="copyright">
+	<p>Copyright (c) 2014 Pearson. All rights reserved. <a href="http://.com/">Jage's Journey</a> | Design by Jage.</p>
+	<ul class="contact">
+		<li><a href="#" class="icon icon-twitter"><span>Twitter</span></a></li>
+		<li><a href="#" class="icon icon-facebook"><span></span></a></li>
+		<li><a href="http://www.edexcel.com" class="icon icon-pqswebsite"><span></span></a></li>
 
+	</ul>
+</div>
+    
 </body>
 </html>
